1b604978ea4ebfe8edc5b189f9d75f84d3dc9ce7
Jonathan <jknahay@yahoo.com>
2025-02-10 20:14:26 -0500

    lil push oo yah

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="footer">
-        <div class="content">
+        <div class="main-footer">
             <div class="paragraph">
                 <p>NHS is a one-person home remodeling and maintenance company dedicated to craftsmanship, attention to detail, and a collaborative approach. With a passion for quality workmanship, I work closely with homeowners to bring their vision to life, ensuring every project—big or small—is completed with care and precision. From renovations to repairs, I focus on creating beautiful, functional spaces while keeping communication open and transparent. Your home deserves the best, and I am here to make it happen—one thoughtful detail at a time.</p>
             </div>
@@ -33,7 +33,7 @@ p {
     display:inline-block;
     /* border-top: 1.5px solid black; */
     width: 100vw;
-    height:fit-content;
+    height:auto;
     /* background-color: #7c8363; */
     background-color: white;
     color: black;
